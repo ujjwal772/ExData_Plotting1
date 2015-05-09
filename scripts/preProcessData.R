feedea4f82f6342495a292245cf8a07997f102c5
ujjwal772 <ujjwal772@gmail.com>
2015-05-09 16:30:15 +0530

    Comment update
    Updated the comment in the script to explain how i got the row indices
    for specific dates. The grep command in bash shell is used for searching
    for a pattern of text in a file.

--- scripts/preProcessData.R
+++ scripts/preProcessData.R
@@ -5,7 +5,7 @@ if(file.exists("household_power_consumption.zip")){
     # Assume the file is already there.
 }
 
-#Subsetting to keep only 1st and 2nd of Feb, 2007
+#Subsetting to keep only 1st and 2nd of Feb, 2007 (got the row numbers using "grep -n")
 ds <- read.csv(file = "household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors=FALSE )[c(66638:69517),]
 
 #Reading Date and combine Date and Time
